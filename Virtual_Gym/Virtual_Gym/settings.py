e82ff65965d22515d20ebb1a84a3250612cf37ff
Jean-marc Lubiba <100803292+jlubiba@users.noreply.github.com>
2024-12-22 16:26:12 -0500

    Connecting the apps with the overall project, doing the first migration, and the creating the first super user.

--- Virtual_Gym/Virtual_Gym/settings.py
+++ Virtual_Gym/Virtual_Gym/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "blog",
+    "practice",
+    "tutorials",
+    "users",
+    "store"
 ]
 
 MIDDLEWARE = [
