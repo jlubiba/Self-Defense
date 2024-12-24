a11e7103eaf6deeb4dfd9a8837e656b659748b6b
Jean-marc Lubiba <100803292+jlubiba@users.noreply.github.com>
2024-12-24 13:23:57 -0500

    creating models for the apps, except the user app.

--- Virtual_Gym/store/models.py
+++ Virtual_Gym/store/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255, db_index=True)
+    
+    def __str__(self) -> str:
+        return self.title
+    
+class ProductList(models.Model):
+    title = models.CharField(max_length = 255, unique=True,db_index = True)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2, db_index = True)
+    featured = models.BooleanField(db_index = True)
+    category = models.ForeignKey(Category, on_delete = models.PROTECT, default=None, related_name='category_name')
+    
+    def __str__(self) -> str:
+        return self.title
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    produclist = models.ForeignKey(ProductList, on_delete= models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits = 6, decimal_places=2)
+    price = models.DecimalField(max_digits = 6, decimal_places=2)
+    
+    class Meta:
+        unique_together = ('produclist', 'user')
+    
+    
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.BooleanField(db_index=True, default=0)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField(db_index=True)
+    delivery_crew = models.ForeignKey(User, on_delete=models.SET_NULL, related_name ="delivery_crew", null=True)
+    
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    produclist = models.ForeignKey(ProductList, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    
+    class Meta:
+        unique_together = ('order', 'produclist')
