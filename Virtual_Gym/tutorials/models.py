251e6c2c60ed997d611751b02c01b96932760f8f
Jean-marc Lubiba <100803292+jlubiba@users.noreply.github.com>
2024-12-26 08:09:08 -0500

    edited the model for the tutorial app, setup a template file, added the video and text tutorial models for the tutorials app, edited a bit of the practice app model as well.

--- Virtual_Gym/tutorials/models.py
+++ Virtual_Gym/tutorials/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 # Create your models here.
 action_types = [('offense', 'Offense'), ('defense', 'Defense'), ('offense & defense', 'Offense & Defense')]
@@ -24,41 +25,31 @@ class Moves(AddedElement):
     class Meta:
         abstract = True
 
-class Category(models.Model):
-    slug = models.SlugField()
-    name = models.CharField(max_length=150)
+class Category(AddedElement):
     description = models.CharField(max_length=250)
-    creation_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
+    added_by= models.ForeignKey(User, on_delete=models.PROTECT,
+                                related_name='tutorials_category_added_by')
+    
+    def __str__(self):
+        return self.name
+
+class Tag(AddedElement):
     added_by= models.ForeignKey(User, on_delete=models.PROTECT)
     
     def __str__(self):
         return self.name
 
-class Tag(models.Model):
-    slug = models.SlugField()
-    name = models.CharField(max_length=150)
-    creation_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
-    added_by= models.ForeignKey(User, on_delete=models.PROTECT)
-    
-    def __str__(self):
-        return self.name
-
-class SubCategory(models.Model):
-    slug = models.SlugField()
-    name = models.CharField(max_length=150)
+class SubCategory(AddedElement):
     description = models.CharField(max_length=250)
     proper_execution = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
-    creation_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
-    added_by= models.ForeignKey(User, on_delete=models.PROTECT)
+    added_by= models.ForeignKey(User, on_delete=models.PROTECT,
+                                related_name='tutorials_subcategory_added_by')
     
     def __str__(self):
         return self.name
     
-class Target(models.Model):
+class Target(Moves):
     name = models.CharField(max_length=150)
     body_level = models.CharField(max_length=50, choices=body_level)
     reason = models.CharField(max_length=250)
@@ -66,27 +57,40 @@ class Target(models.Model):
     def __str__(self):
         return self.name
     
-class Technique(models.Model):
+class Technique(Moves):
     name = models.CharField(max_length=150)
     steps = models.TextField()
     application = models.CharField(max_length=250)
-    sub_category = models.ForeignKey(SubCategory, on_delete=models.PROTECT)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.PROTECT,
+                                related_name='tutorials_technique_subcategory')
     target = models.ManyToManyField(Target, related_name='tutorials_technique_target')
-    body_level = models.CharField(max_length=50, choices=body_level)
-    action_types = models.CharField(max_length=50, choices=action_types)
-    difficty_level = models.CharField(max_length=50, choices=difficty_level)
     
     def __str__(self):
         return self.name
     
-class Combo(models.Model):
+class Combo(Moves):
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=250, default='A cool combo!')
     technique = models.ManyToManyField(Technique, related_name='technique')
     target = models.ManyToManyField(Target, related_name='tutorials_combo_target')
-    body_level = models.CharField(max_length=50, choices=body_level)
-    action_types = models.CharField(max_length=50, choices=action_types)
-    difficty_level = models.CharField(max_length=50, choices=difficty_level)
     
+    def __str__(self):
+        return self.name
+
+class TextTutorial(AddedElement):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    body = RichTextField(blank=True, null=True)
+    sub_category = models.ManyToManyField(SubCategory, related_name='sub_category')
+    tag = models.ManyToManyField(Tag, related_name='tag')
+    
+    def __str__(self):
+        return self.name
+
+class VideoTutorial(AddedElement):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="videos")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    image = models.ImageField(upload_to='static/images/tutorials')
+    url = models.URLField()
+
     def __str__(self):
         return self.name
